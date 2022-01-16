5f9f1659013e0682e24917d1af8ef45027c7b36b
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-01-16 20:28:43 +0300

    Initial Commit

---
+++ src/main/java/io/github/mityavasilyev/springvertxreactbarapp/SpringVertxReactBarAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.mityavasilyev.springvertxreactbarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVertxReactBarAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVertxReactBarAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/mityavasilyev/springvertxreactbarapp/SpringVertxReactBarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mityavasilyev.springvertxreactbarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVertxReactBarAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
