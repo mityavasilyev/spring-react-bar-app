14e04293cb6e4c7adfee4a236221097802f57f22
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-01-22 02:08:05 +0300

    Cleaned up main class a bit

--- src/main/java/io/github/mityavasilyev/springvertxreactbarapp/SpringVertxReactBarAppApplication.java
+++ src/main/java/io/github/mityavasilyev/springvertxreactbarapp/SpringVertxReactBarAppApplication.java
@@ -1,12 +1,7 @@
 package io.github.mityavasilyev.springvertxreactbarapp;
 
-import io.github.mityavasilyev.springvertxreactbarapp.model.Cocktail;
-import io.github.mityavasilyev.springvertxreactbarapp.model.Ingredient;
-import io.github.mityavasilyev.springvertxreactbarapp.model.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.HashSet;
 
 @SpringBootApplication
 public class SpringVertxReactBarAppApplication {
