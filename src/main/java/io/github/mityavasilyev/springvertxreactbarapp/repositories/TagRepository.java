3db59ef68f5dcd4354fcfee158e1ca17815fca4e
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-01-22 13:34:18 +0300

    Added queries to TagRepository. Closes #10

--- src/main/java/io/github/mityavasilyev/springvertxreactbarapp/repositories/TagRepository.java
+++ src/main/java/io/github/mityavasilyev/springvertxreactbarapp/repositories/TagRepository.java
@@ -4,6 +4,10 @@ import io.github.mityavasilyev.springvertxreactbarapp.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    List<Tag> findByNameContainingIgnoreCase(String name);
 }
