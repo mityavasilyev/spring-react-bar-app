9a2bede8e3f21917e0a3cf6d5f5c1fa7a703fd26
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-01-22 13:47:50 +0300

    Added queries to ProductRepository. Closes #9
    Plus documentation

--- src/main/java/io/github/mityavasilyev/springvertxreactbarapp/repositories/ProductRepository.java
+++ src/main/java/io/github/mityavasilyev/springvertxreactbarapp/repositories/ProductRepository.java
@@ -2,6 +2,18 @@ package io.github.mityavasilyev.springvertxreactbarapp.repositories;
 
 import io.github.mityavasilyev.springvertxreactbarapp.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /**
+     * Searches repository for products that have Name containing provided string
+     *
+     * @param name
+     * @return matching cocktails
+     */
+    List<Product> findByNameContainingIgnoreCase(String name);
 }
--- src/main/java/io/github/mityavasilyev/springvertxreactbarapp/repositories/TagRepository.java
+++ src/main/java/io/github/mityavasilyev/springvertxreactbarapp/repositories/TagRepository.java
@@ -9,5 +9,11 @@ import java.util.List;
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
+    /**
+     * Searches repository for tags that have Name containing provided string
+     *
+     * @param name
+     * @return matching cocktails
+     */
     List<Tag> findByNameContainingIgnoreCase(String name);
 }
