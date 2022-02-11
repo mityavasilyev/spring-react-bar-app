3816c64b83378037a2c71334152604a570a56cba
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-02-11 20:07:17 +0300

    AuthService interface fix
    CI build test workflow is already paying off

--- src/main/java/io/github/mityavasilyev/springreactbarapp/security/AuthService.java
+++ src/main/java/io/github/mityavasilyev/springreactbarapp/security/AuthService.java
@@ -1,7 +1,5 @@
 package io.github.mityavasilyev.springreactbarapp.security;
 
-import io.github.mityavasilyev.springreactbarapp.exceptions.NoSuchRoleException;
-import io.github.mityavasilyev.springreactbarapp.exceptions.NoSuchUserException;
 import io.github.mityavasilyev.springreactbarapp.security.appuser.AppUser;
 import io.github.mityavasilyev.springreactbarapp.security.role.Role;
 
@@ -10,10 +8,15 @@ import java.util.List;
 public interface AuthService {
 
     AppUser saveUser(AppUser appUser);
+
     Role saveRole(Role role);
-    void assignRoleToUser(String username, Long roleId) throws NoSuchRoleException, NoSuchUserException;
-    AppUser getUser(Long id) throws NoSuchUserException;
-    Role getRole(Long id) throws NoSuchRoleException;
+
+    void assignRoleToUser(String username, Long roleId);
+
+    AppUser getUser(Long id);
+
+    Role getRole(Long id);
+
     List<AppUser> getUsers();
 
 }
