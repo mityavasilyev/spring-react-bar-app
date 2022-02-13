a85ab02c0913e06a0a4dbc732a7aebc7c4cf914b
Dmitry <contact.dmitry.vasilyev@gmail.com>
2022-02-14 00:32:14 +0300

    JWT Tokens generation kinda done

--- src/main/java/io/github/mityavasilyev/springreactbarapp/security/CustomAuthenticationFilter.java
+++ src/main/java/io/github/mityavasilyev/springreactbarapp/security/CustomAuthenticationFilter.java
@@ -1,10 +1,14 @@
 package io.github.mityavasilyev.springreactbarapp.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,10 +16,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    // Pass down this value from somewhere
+    public static final String SECRET = "secret";
     private final AuthenticationManager authenticationManager;
 
     public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
@@ -33,7 +42,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         log.info("New login attempt with username/password [{}/{}]", username, password);
 
         UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(username,password);
+                new UsernamePasswordAuthenticationToken(username, password);
 
         return authenticationManager.authenticate(authenticationToken);
     }
@@ -46,7 +55,27 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
             Authentication authResult) throws IOException, ServletException {
         log.info("Successful login attempt with username [{}]", request.getParameter("username"));
 
-        super.successfulAuthentication(request, response, chain, authResult);
+        User user = (User) authResult.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256(SECRET.getBytes(StandardCharsets.UTF_8));
+
+        String accessToken = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + (10 * 60 * 1000)))
+                .withIssuer(request.getRequestURI().toString())
+                .withClaim("roles",
+                        user.getAuthorities().stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .collect(Collectors.toList()))
+                .sign(algorithm);
+
+        String refreshToken = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + (30 * 60 * 1000)))
+                .withIssuer(request.getRequestURI().toString())
+                .sign(algorithm);
+
+        response.setHeader("access_token", accessToken);
+        response.setHeader("refresh_token", refreshToken);
     }
 
     // TODO: 12.02.2022 configure login denial
